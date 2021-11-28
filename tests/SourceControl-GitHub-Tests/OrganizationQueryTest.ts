e8c0fad260d303d42c6a6aa7cacb01efff0be078
robertDurst <me@robdurst.com>
2021-11-28 17:17:19 -0500

    base test for github integration

--- tests/SourceControl-GitHub-Tests/OrganizationQueryTest.ts
+++ tests/SourceControl-GitHub-Tests/OrganizationQueryTest.ts
@@ -4,14 +4,14 @@ import {
 } from "../../src/SourceControl-GitHub/Organization";
 import { expect } from "chai";
 
+const thisOrgName = "wondrous-dev";
+const thisRepoName = "source-control-integrations";
+const nonExistantRepoName = "go-programming-language";
+
 describe("Properly Queries Organization", async () => {
-  const org: Organization = await GetOrganization("wondrous-dev");
-
-  it("contains this repo", () => {
-    expect(org.containsRepo("source-control-integrations")).to.equal(true);
-  });
-
-  it("does not contain go programming language repo", () => {
-    expect(org.containsRepo("go")).to.equal(false);
+  it("wondrous-dev contains 'source-control-integrations' but not 'go-programming-language'", async () => {
+    const org: Organization = await GetOrganization(thisOrgName);
+    expect(org.containsRepo(thisRepoName)).to.equal(true);
+    expect(org.containsRepo(nonExistantRepoName)).to.equal(false);
   });
 });
